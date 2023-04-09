335358a8d7fd6940c05c28afef9b9135397801df
sploganathan <sakthiloganathan1127@gmail.com>
2023-04-09 05:16:47 -0700

    feat(project): created project component structure

---
+++ src/components/Project.js
@@ -0,0 +1,122 @@
+function Project() {
+  return (
+    <div className="containers">
+      <div className="row m-3">
+        <div className="col-md-4 col-lg-4 col-sm-12 mb-3">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Project title</h5>
+              <h6 className="card-subtitle mb-2 text-muted">
+                Project subtitle
+              </h6>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" className="card-link">
+                Card link
+              </a>
+              <a href="#" className="card-link">
+                Another link
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4 col-lg-4 col-sm-12 mb-3">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" className="card-link">
+                Card link
+              </a>
+              <a href="#" className="card-link">
+                Another link
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4 col-lg-4 col-sm-12 mb-3">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" className="card-link">
+                Card link
+              </a>
+              <a href="#" className="card-link">
+                Another link
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row m-3">
+        <div className="col-md-4 col-lg-4 col-sm-12 mb-3">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" className="card-link">
+                Card link
+              </a>
+              <a href="#" className="card-link">
+                Another link
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4 col-lg-4 col-sm-12 mb-3">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" className="card-link">
+                Card link
+              </a>
+              <a href="#" className="card-link">
+                Another link
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4  col-lg-4 col-sm-12 mb-3">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+              <p class="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" class="card-link">
+                Card link
+              </a>
+              <a href="#" class="card-link">
+                Another link
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Project;
