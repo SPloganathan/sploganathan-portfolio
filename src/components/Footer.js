f84abdc49cad70fb8f2646db0447d9b40ee7028c
sploganathan <sakthiloganathan1127@gmail.com>
2023-04-10 11:35:44 -0700

    feat(footer) : footer component and downloable resume added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,13 +7,31 @@ function Footer() {
           <div className="col-12">
             <div className="d-flex flex-row justify-content-center">
               <div className="mx-2 text-uppercase">
-                <Icon icon="mdi:github" />
+                <a
+                  href="https://github.com/sploganathan"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <Icon icon="mdi:github" />
+                </a>
               </div>
               <div className="mx-2 text-uppercase">
-                <Icon icon="mdi:linkedin" />
+                <a
+                  href="https://www.linkedin.com/in/sakthipriya-loganathan/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <Icon icon="mdi:linkedin" />
+                </a>
               </div>
               <div className="mx-2 text-uppercase">
-                <Icon icon="mdi:stackoverflow" />
+                <a
+                  href="https://stackoverflow.com/users/20064410/sakthi"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <Icon icon="mdi:stackoverflow" />
+                </a>
               </div>
             </div>
           </div>
