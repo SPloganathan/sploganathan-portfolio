e17f41f13c78fb1a1aa18d17b50c0a5193259c00
sploganathan <sakthiloganathan1127@gmail.com>
2023-04-10 19:08:01 -0700

    feat(resume): resume component done

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -13,7 +13,24 @@ function Resume() {
         </a>
       </div>
       <div className="row">
-        <h2>Front end Proficiency</h2>
+        <h2>Front-end Proficiencies</h2>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>JQuery</li>
+          <li>React</li>
+          <li>Bootstrap</li>
+        </ul>
+        <h2>Back-end Proficiencies</h2>
+        <ul>
+          <li>APIs</li>
+          <li>Node</li>
+          <li>Express</li>
+          <li>MySQL,Sequelize</li>
+          <li>NoSQL,MongoDB</li>
+          <li>GraphQL</li>
+        </ul>
       </div>
     </div>
   );
